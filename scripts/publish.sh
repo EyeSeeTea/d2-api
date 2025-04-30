6bdcfa8f254f7896dcc8ae46e0630bb2ad366a11
Arnau Sanchez <pyarnau@gmail.com>
2025-04-30 10:18:14 +0200

    Pass options to publish script for extension

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,7 +5,7 @@ version=$(cat package.json | jq -r '.version')
 publish_opts=$(if echo "$version" | grep -q beta; then echo "--tag beta"; fi)
 
 yarn build
-yarn publish $publish_opts --new-version "$version" build/
+yarn publish $publish_opts "${@}" --new-version "$version" build/
 
 git tag "v$version" -f -m "Bump version"
 git push --tags
